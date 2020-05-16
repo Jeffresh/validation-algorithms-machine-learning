7cbfac2a235f7261408cc4ca662da42a80703ed1
Jeff <jeff.uca@gmail.com>
2020-05-16 20:32:29 +0200

    :sparkles: add function to evalue model

---
+++ predict.m
@@ -0,0 +1,20 @@
+function h = predict(input,theta)
+%PREDICT Summary of this function goes here
+%   Detailed explanation goes here
+
+len = length(theta)-1;
+
+if len < 1
+    len = 1;
+else
+    input = repmat(input,len,1);
+    for i =1:len
+        input(i,:)= input(i,:).^i;
+    end
+    
+end
+
+
+h =[ones(1,size(input,2));input]' *theta;
+
+end
