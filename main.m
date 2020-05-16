1b4a98b291301e687d04952cc9e2e89b00632b6a
Jeff <jeff.uca@gmail.com>
2020-05-16 23:22:08 +0200

    :sparkles: grade is now a variable

--- main.m
+++ main.m
@@ -8,15 +8,17 @@ y = exp(x.^3 - x.^2 + 0.01*x + 2) + 0.04 * randn(size(x));
 plot_data(x,y)
 
 
-%%
+%% Polynomial model
+
+grade = 2;
 
 points = linspace(0,1.5,100);
 
-theta = train(x,y,1);
+theta = train(x,y,grade);
 
-xtest = predict(points, theta);
+ypredicted = predict(points, theta);
 
 plot_data(x,y), hold on
-plot(points,xtest, '-');hold off
-legend('Training data', 'Linear regression')
+plot(points,ypredicted, '-');hold off
+legend('Training data', ['Polynomial regression Grade:' num2str(grade,'%d')])
 
