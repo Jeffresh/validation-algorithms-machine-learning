bea9f8b9c8635493549c0ea2a97728f8e4a6efc5
Jeff <jeff.uca@gmail.com>
2020-05-17 14:08:35 +0200

    :sparkles: add function to computes generalization error over simple validation

---
+++ simple_validation.m
@@ -0,0 +1,24 @@
+function generalization_error = simple_validation(x,y,test_data_percent, train_function, predict_function, grade)
+%SIMPLE_VALIDATION Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+trn_data_length = round((length(x) * test_data_percent)/100,0);
+data_indexes = randperm(length(x));
+
+xtrn = x(data_indexes(1:trn_data_length));
+xtest = x(data_indexes(trn_data_length+1:end));
+
+ytrn = y(data_indexes(1:trn_data_length));
+ytest = y(data_indexes(trn_data_length+1:end));
+
+if nargin>4
+  model = train_function(xtrn,ytrn,grade);
+else
+  model = train_function(xtrn,ytrn,grade);
+end
+
+generalization_error = cost_function(xtest,ytest,model, predict_function);
+end
+
