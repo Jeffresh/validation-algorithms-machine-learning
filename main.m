462596d45cb0ab3249c7e740a5d19b18bfb86bcf
Jeff <jeff.uca@gmail.com>
2020-05-17 21:41:19 +0200

    :sparkles: add leave one out to the three models

--- main.m
+++ main.m
@@ -77,7 +77,6 @@ mean_error=(mean(error));
 
 %% Simple validation to a linear model  a + bx + x^2 + dx^3+ e*sin(x) + f*sin(x)
 
-
 max_iterations = 1000;
 test_data_percent = 70;
 
@@ -105,7 +104,7 @@ legend('generalization error')
 xlabel('polynomial grade')
 
 %% Cross validation to a linear model  a + bx + x^2 + dx^3+ e*sin(x)
-max_polynom_grade = 30;
+
 max_iterations = 23;
 order = 10;
 
@@ -118,7 +117,6 @@ mean_error=(mean(error))
 
 %% Cross validation to a linear model  a + bx + x^2 + dx^3+ e*sin(x) + f*sin(x)
 
-max_polynom_grade = 30;
 max_iterations = 23;
 order = 10;
 
@@ -127,4 +125,45 @@ error(i) = kross_validation(x,y,order, @linear_model_2_train, @linear_model_2_pr
 end
 
 mean_error=(mean(error))
+
+
+%% leave one out validation polynomial adjustment
+
+max_iterations = 23;
+order = 10;
+
+for polynom = 1:max_polynom_grade
+    for i = 1:max_iterations
+    error(i) = leave_one_out(x,y,order, @polynomial_train, @polynomial_predict,polynom);
+    end
+    mean_error(polynom)=(mean(error));
+end
+
+plot(mean_error)
+legend('generalization error')
+xlabel('polynomial grade')
+
+%% One leave out to a linear model  a + bx + x^2 + dx^3+ e*sin(x)
+
+max_iterations = 23;
+order = 10;
+
+for i = 1:max_iterations
+error(i) = leave_one_out(x,y,order, @linear_model_1_train, @linear_model_1_predict);
+end
+
+mean_error=(mean(error))
+
+%% Leave one out validation to a linear model  a + bx + x^2 + dx^3+ e*sin(x) + f*sin(x)
+
+max_iterations = 23;
+order = 10;
+
+for i = 1:max_iterations
+error(i) = leave_one_out(x,y,order, @linear_model_2_train, @linear_model_2_predict);
+end
+
+mean_error=(mean(error))
+
+
 
